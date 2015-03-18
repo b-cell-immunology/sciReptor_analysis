599859868652dd42344277a9c8623da487368cc3
Christian Busse <christian.busse@dkfz-heidelberg.de>
2015-03-18 19:27:38 +0100

    SQL/create_temp_table_VDJ.sql: To reduce the data volume the segment assignment of the second igblast_rank will now be ignored. "JOIN ... ON locus" has also been removed, since the subqueries are filtered on a given locus anyhow.
    SQL/create_segments_view.sql: Renamed from "segments_view.sql". The
    function now performs automatic filtering, returning only sequences
    with functional (i.e. non-stop) CDR3s. In addition, the function will
    only use the sequence with the lowest consensus_rank number (which is
    functional, as mentioned above), i.e. consensus_rank 2 will be ignored
    if consensus_rank 1 is functional, but will be used if rank 1 is non-
    functional.

--- SQL/create_temp_table_VDJ.sql
+++ SQL/create_temp_table_VDJ.sql
@@ -1,8 +1,14 @@
-# Create temporary tables containing the connected V(D)J joints.
+# Name:			create_temp_table_VDJ.sql
+# Verson:		0.1.1  (2015-03-18)
+# Author(s):	Christian Busse
+# Maintainer:	Christian Busse (christian.busse@dkfz-heidelberg.de)
+# Licence:		AGPL3
+# Provides:		Temporary tables containing the connected V(D)J joints for all three loci.
 #
 
-# === Heavy ===
 
+# Create table for heavy chain
+#
 DROP TABLE IF EXISTS temp_table_H_VDJ, temp_table_H_VJ, temp_table_H_D, temp_table_H_C;
 
 CREATE TEMPORARY TABLE temp_table_H_VJ AS (
@@ -10,15 +16,12 @@ CREATE TEMPORARY TABLE temp_table_H_VJ AS (
 		v_segment.seq_id AS seq_id,
 		v_segment.name AS v_segment_name,
 		j_segment.name AS j_segment_name,
-		v_segment.igblast_rank AS igblast_rank,
 		v_segment.locus AS locus
 	FROM 
-	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='V' AND locus='H') AS v_segment
+	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='V' AND locus='H' AND igblast_rank=1) AS v_segment
 	INNER JOIN
-	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='J' AND locus='H') AS j_segment
+	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='J' AND locus='H' AND igblast_rank=1) AS j_segment
 	ON v_segment.seq_id = j_segment.seq_id
-	AND v_segment.igblast_rank = j_segment.igblast_rank
-	AND v_segment.locus = j_segment.locus
 );
 
 ALTER TABLE temp_table_H_VJ ADD COLUMN d_segment_name VARCHAR(20) AFTER v_segment_name;
@@ -28,11 +31,11 @@ CREATE TEMPORARY TABLE temp_table_H_D AS (
 	SELECT 
 		seq_id,
 		`name` AS d_segment_name,
-		igblast_rank,
 		locus
 	FROM VDJ_segments
     WHERE type='D'
-	AND locus='H'
+		AND locus='H'
+		AND igblast_rank=1
 );
 
 CREATE TEMPORARY TABLE temp_table_H_C AS (
@@ -44,9 +47,7 @@ CREATE TEMPORARY TABLE temp_table_H_C AS (
 
 SET SQL_SAFE_UPDATES=0;
 UPDATE temp_table_H_VJ, temp_table_H_D SET temp_table_H_VJ.d_segment_name = temp_table_H_D.d_segment_name
-WHERE temp_table_H_VJ.seq_id = temp_table_H_D.seq_id
-AND temp_table_H_VJ.igblast_rank = temp_table_H_D.igblast_rank
-AND temp_table_H_VJ.locus = temp_table_H_D.locus;
+WHERE temp_table_H_VJ.seq_id = temp_table_H_D.seq_id;
 
 UPDATE temp_table_H_VJ, temp_table_H_C SET temp_table_H_VJ.c_segment_name = temp_table_H_C.c_segment_name
 WHERE temp_table_H_VJ.seq_id = temp_table_H_C.seq_id;
@@ -56,8 +57,9 @@ DROP TABLE IF EXISTS temp_table_H_D, temp_table_H_C;
 
 ALTER TABLE temp_table_H_VJ RENAME temp_table_H_VDJ;
 
-# === Kappa ===
 
+# Create tables for kappa chain
+#
 DROP TABLE IF EXISTS temp_table_K_VJ, temp_table_K_C;
 
 CREATE TEMPORARY TABLE temp_table_K_VJ AS (
@@ -65,15 +67,12 @@ CREATE TEMPORARY TABLE temp_table_K_VJ AS (
 		v_segment.seq_id AS seq_id,
 		v_segment.name AS v_segment_name,
 		j_segment.name AS j_segment_name,
-		v_segment.igblast_rank AS igblast_rank,
 		v_segment.locus AS locus
 	FROM 
-	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='V' AND locus='K') AS v_segment
+	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='V' AND locus='K' AND igblast_rank=1) AS v_segment
 	INNER JOIN
-	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='J' AND locus='K') AS j_segment
+	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='J' AND locus='K' AND igblast_rank=1) AS j_segment
 	ON v_segment.seq_id = j_segment.seq_id
-	AND v_segment.igblast_rank = j_segment.igblast_rank
-	AND v_segment.locus = j_segment.locus
 );
 
 ALTER TABLE temp_table_K_VJ ADD COLUMN c_segment_name VARCHAR(20) AFTER j_segment_name;
@@ -94,8 +93,9 @@ SET SQL_SAFE_UPDATES=1;
 
 DROP TABLE IF EXISTS temp_table_K_C;
 
-# === Lambda ===
 
+# Create tables for lambda chain
+#
 DROP TABLE IF EXISTS temp_table_L_VJ, temp_table_L_C;
 
 CREATE TEMPORARY TABLE temp_table_L_VJ AS (
@@ -103,15 +103,12 @@ CREATE TEMPORARY TABLE temp_table_L_VJ AS (
 		v_segment.seq_id AS seq_id,
 		v_segment.name AS v_segment_name,
 		j_segment.name AS j_segment_name,
-		v_segment.igblast_rank AS igblast_rank,
 		v_segment.locus AS locus
 	FROM 
-	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='V' AND locus='L') AS v_segment
+	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='V' AND locus='L' AND igblast_rank=1) AS v_segment
 	INNER JOIN
-	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='J' AND locus='L') AS j_segment
+	(SELECT seq_id, `name`, igblast_rank, locus FROM VDJ_segments WHERE type='J' AND locus='L' AND igblast_rank=1) AS j_segment
 	ON v_segment.seq_id = j_segment.seq_id
-	AND v_segment.igblast_rank = j_segment.igblast_rank
-	AND v_segment.locus = j_segment.locus
 );
 
 ALTER TABLE temp_table_L_VJ ADD COLUMN c_segment_name VARCHAR(20) AFTER j_segment_name;
